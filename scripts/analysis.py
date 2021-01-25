797a47769e8d45b6d1f3aa88924f89667a094ebc
jmoldon <jmoldon@iaa.es>
2021-01-25 12:44:57 +0000

    Add Sofia execution

--- scripts/analysis.py
+++ scripts/analysis.py
@@ -5,8 +5,13 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import yaml
 
-data_parameters = 'parameters/data.yml'
-data_path = './data'
+data_parameters = './parameters/data.yml'
+param_development_small = './parameters/sofia_dev_small.par'
+
+data_path = 'data'
+results_path = 'results'
+
+dev_small_cat = './results/development_small/developement_small_cat.txt'
 
 def download_data(data_parameters, force=False):
     if not os.path.isdir(data_path):
@@ -17,23 +22,45 @@ def download_data(data_parameters, force=False):
 
     for dataset in data_yml.keys():
         print(dataset)
+        dataset_dir = os.path.join(data_path, dataset)
         if force == True:
-            shutil.rmtree(f"{data_path}/{dataset}")
+            shutil.rmtree(dataset_dir)
 
-        if not os.path.isdir(f"{data_path}/{dataset}"):
+        if not os.path.isdir(dataset_dir):
             pathname = data_yml[dataset]['path']
-            os.mkdir(f"{data_path}/{dataset}")
+            os.mkdir(dataset_dir)
             for filename in data_yml[dataset]['files']:
-                command = f'wget --no-check-certificate "{pathname} download?path=%2F&files={filename}" -O {data_path}/{dataset}/{filename}'
+                command = f'wget --no-check-certificate "{pathname}download?path=%2F&files={filename}" -O {dataset_dir}/{filename}'
                 print(command)
                 os.system(command)
 
-download_data(data_parameters)
+def run_sofia(parameters, outputdir):
+    """Only executed if directory does not exist"""
+    if not os.path.isdir(results_path):
+        os.mkdir(results_path)
+        
+    if not os.path.isdir(os.path.join(results_path, outputdir)):
+        os.mkdir(os.path.join(results_path, outputdir))
+        command = f"sofia {parameters}"
+        print(command)
+        os.system(command)
+    return
+
+def sofia2cat(catalog):
+    return 
+
+def main():
+    download_data(data_parameters, force=False)
+    run_sofia(parameters=param_development_small,
+              outputdir='development_small')
+    sofia2cat(catalog=dev_small_cat)
+    
+if __name__ == "__main__":                
+    main()
 
 ## Download data
 #is os.path.isfile
 #cat_file = './test2/sofia_test_output_cat.txt'
-#
 #truth_file = '../data/development/sky_dev_truthcat.txt'
 #truth = pd.read_csv(truth_file, delim_whitespace=True)
 #
